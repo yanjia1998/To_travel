21230ccdd3474e436d53eb8126ee95723117e27c
yanjia <475581796@qq.com>
2020-12-12 17:29:13 +0800

    travel

---
+++ src/main/java/com/qf/SpringTravelApplication.java
@@ -0,0 +1,12 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTravelApplication.class);
+    }
+}
